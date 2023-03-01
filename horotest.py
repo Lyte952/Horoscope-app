f76a44f7ab05d2fb865fd4add6fe39f56fb780a7
Lyte952 <lytecodes@gmail.com>
2023-03-01 23:32:38 +0000

    Python file of horoscope.py pushed

---
+++ horotest.py
@@ -0,0 +1,28 @@
+import requests
+import datetime
+
+# Username is your own name(first or last)
+username = input("Enter your name: ")
+day = datetime.datetime.now()
+
+def main():
+    horoscope_message_ = horoscope_message()
+    print(day)
+    print()
+    with open(f"({day}).txt", 'wb') as file:
+        file.write(horoscope_message())
+
+# Returns your horoscope sign name
+def horoscope_name():
+    name = input("Please enter your horoscope name: ")
+    return name
+
+# request code that pulls up your current horoscope message from the website
+def horoscope_message():
+    url = f'https://www.astrosage.com/horoscope/daily-{horoscope_name()}-horoscope.asp'
+    r = requests.get(url)
+    return r.json
+
+
+if __name__=="__main__":
+    main() 
